56cfcc3f50f52053813c57b3fb94b30064aad6ed
Denis Stankevich <denisstankevich24@gmail.com>
2024-07-17 20:20:53 +0300

    add i3, polybar, picom configs

--- nvim/.config/nvim/lua/config/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-		config = function()
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		lazy = true,
-		name = "catppuccin",
-		opts = {
-			flavor = "mocha",
-			integrations = {
-				alpha = true,
-				cmp = true,
-				gitsigns = true,
-				indent_blankline = { enabled = true },
-				lsp_trouble = true,
-				mason = true,
-				markdown = true,
-				mini = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
-				navic = { enabled = true, custom_bg = "lualine" },
-				neotree = true,
-				noice = true,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = true,
-				harpoon = true,
-				which_key = true,
-			},
-		},
-		priority = 1000,
-	},
+  {
+    "EdenEast/nightfox.nvim",
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      flavor = "mocha",
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotree = true,
+        noice = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        harpoon = true,
+        which_key = true,
+      },
+    },
+    priority = 1000,
+  },
 }
---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,2 @@
+pkill polybar && sleep 1
+polybar -c ~/.config/polybar/config.ini &
